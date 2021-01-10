1487dc7019aaa4a33e9f46669dfb9d628cfb1ac0
Rakesh <rakesh.phpdeveloper@gmail.com>
2021-01-10 18:44:23 +0530

    partner node form logic

--- web/modules/custom/custom_survey/js/node_survey_partner.js
+++ web/modules/custom/custom_survey/js/node_survey_partner.js
@@ -12,29 +12,42 @@
    */
   Drupal.behaviors.jsSurveyPartnerFieldType = {
     attach: function (context, settings) {
-      var textFieldKey = 'field_survey_link[0][value]';
-      var replaceFieldKey = 'field_identifier_of_survey_link[0][value]';
-      var radioFieldContainer = 'field_survey_link_radio-field';
-      var radioFieldKey = 'radio_' + textFieldKey;
-      $('input[name="' + textFieldKey + '"]').once().on('paste, keydown', function () {
-        $('.' + radioFieldContainer).html('');
-        var element = this;
-        setTimeout(function () {
-          var text = $.trim($(element).val());
-          $(element).val(text);
-          getUrlVars(text, radioFieldKey, radioFieldContainer);
-
-        }, 100);
-      });
-      $(document).once().on('click', 'input[name="' + radioFieldKey + '"]', function () {
-        $('input[name="' + replaceFieldKey + '"]').val($(this).val());
-      });
+      var field_list = {
+        'field_survey_link':'field_identifier_of_survey_link',
+        'field_completes_redirect':'field_identifier_of_completes',
+        'field_quota_full_redirect':'field_identifier_of_quota_full',
+        'field_terminates_redirect':'field_identifier_of_terminates'
+      };
+      $.each(field_list, function( key_name, identifier ) {
+        var textFieldKey = key_name+'[0][value]';
+        var replaceFieldKey = identifier+'[0][value]';
+        var radioFieldContainer = key_name+'_radio-field';
+        var radioFieldKey = 'radio_' + key_name;
+        $('input[name="' + textFieldKey + '"]').once().on('paste, keydown', function (e) {
+          switch(e.which) {
+            case 37: // left
+            case 38: // up
+            case 39: // right
+            case 40: // down
+            break;
+            default:
+            $('.' + radioFieldContainer).html('');
+            var element = this;
+            setTimeout(function () {
+              var text = $.trim($(element).val());
+              $(element).val(text);
+              getUrlVars(text, radioFieldKey, radioFieldContainer, replaceFieldKey);
+            }, 100);
+          }
+        });
+      });
     }
   };
 })(jQuery, Drupal, drupalSettings);
 
-function getUrlVars(urlstring, radioFieldKey, radioFieldContainer)
+function getUrlVars(urlstring, radioFieldKey, radioFieldContainer, replaceFieldKey)
 {
+
   var vars = [], hash;
   var hashes = urlstring.slice(urlstring.indexOf('?') + 1).split('&');
   for (var i = 0; i < hashes.length; i++)
@@ -46,12 +59,16 @@ function getUrlVars(urlstring, radioFieldKey, radioFieldContainer)
       jQuery('<div><input id="' + radioBoxKey + '" type="radio" name="' + radioFieldKey + '" value="' + hash[0] + '" /><label class="option" for="' + radioBoxKey + '">' + hash[0] + '</label></div>').appendTo('.' + radioFieldContainer);
     }
   }
-  return vars;
+  jQuery('input[name="'+radioFieldKey+'"]').once().on('click', function () {
+    jQuery('input[name="'+replaceFieldKey+'"]').val(jQuery(this).val());
+  });
+  return vars;
 }
 
 (function($) {
   $.fn.ajaxAutoSelectedFieldMethod = function(data) {
-    $('input[name="field_mobile[0][value]"]').val(data[0]);
-    $('input[name="field_name[0][value]"]').val(data[1]);
+    $.each(data, function( index, value ) {
+      $('input[name="'+index+'[0][value]"]').val(value);
+    });
   };
 })(jQuery);
