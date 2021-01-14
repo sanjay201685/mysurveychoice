20dd79d1eef39a0b2613c475981d6eb5d9c93906
Sanjay <sky.creatrix@gmail.com>
2021-01-15 01:59:56 +0530

    Added validation

--- web/surveys/partner_survey.php
+++ web/surveys/partner_survey.php
@@ -2,7 +2,7 @@
 require_once 'connection.php';
 session_start();
 $partner_id =  (string) filter_input(INPUT_GET, 'id');
-$survey_id = (string) filter_input(INPUT_GET, 'srv');
+$survey_map_id = (string) filter_input(INPUT_GET, 'srv');
 
 $sessionid = session_id();
 function getToken($length){
@@ -18,51 +18,96 @@ function getToken($length){
 
     return $token;
 }
-$id = "CR-".$survey_id."_".getToken(20);  
-function Redirect($url, $permanent = false)
-    {
-    header('Location: ' . $url, true, $permanent ? 301 : 302);
-    exit();
-    }
-if ($id == "xxxx" || !$id || !$survey_id) {
-    echo "Invalid Parameters";
-    exit;
+
+// Dynamic respondent id
+$id = "CR-".$survey_map_id."_".getToken(20);  
+/* Validate required survey for a partner completed - Start here */
+$query = "SELECT com.field_completes_value AS cv, sr.field_total_survey_required_value AS rs  
+          FROM node__field_total_survey_required AS sr
+          JOIN node__field_completes AS com ON sr.entity_id = com.entity_id
+          WHERE sr.entity_id = ? ";
+$result = $conn->prepare($query);
+$result->execute([$survey_map_id]);
+$result1 = $result->fetchAll(PDO::FETCH_ASSOC);
+
+if ($result1[0]['cv'] > $result1[0]['rs']) {
+  $survey_link = getRedurectUrl($conn, $survey_map_id);
+  $survey_link = str_replace("xxxx", $id, $survey_link);
+  Redirect($survey_link, false);
+  die;  
 }
+/* Validate required survey for a partner completed - End here */
+
+function Redirect($url, $permanent = false) {
+  header('Location: ' . $url, true, $permanent ? 301 : 302);
+  exit();
+}
+
+if ($id == "xxxx" || !$id || !$survey_map_id) {
+  echo "Invalid Parameters";
+  exit;
+}
+
 $geopIp = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR']));
-	$u_location = $geopIp['geoplugin_city']." ";
-	$u_location .= $geopIp['geoplugin_regionName']." ";
-	$u_location .= $geopIp['geoplugin_countryName']." ";
-	$u_location .= $geopIp['geoplugin_areaCode']." ";
-	if(empty($u_location) ){
-		$geopIp = unserialize(file_get_contents('https://freegeoip.app/json/'.$_SERVER['REMOTE_ADDR']));
-		$u_location = $geopIp['city']." ";
-		$u_location .= $geopIp['region_name']." ";
-		$u_location .= $geopIp['country_name']." ";
-		$u_location .= $geopIp['zip_code']." ";
-	}
-if ($survey_id) {
+$u_location = $geopIp['geoplugin_city']." ";
+$u_location .= $geopIp['geoplugin_regionName']." ";
+$u_location .= $geopIp['geoplugin_countryName']." ";
+$u_location .= $geopIp['geoplugin_areaCode']." ";
+
+if (empty($u_location) ){
+  $geopIp = unserialize(file_get_contents('https://freegeoip.app/json/'.$_SERVER['REMOTE_ADDR']));
+  $u_location = $geopIp['city']." ";
+  $u_location .= $geopIp['region_name']." ";
+  $u_location .= $geopIp['country_name']." ";
+  $u_location .= $geopIp['zip_code']." ";
+}
+
+if ($survey_map_id) {
     try {
-        $ins_data['id'] = $id;
+        $ins_data['resp_id'] = $id;
         $ins_data['partner_id'] = $partner_id;
-        $ins_data['sessionid'] = $sessionid;
-        $ins_data['survey_id'] = $survey_id;
-        $ins_data['u_location'] = $u_location;
+        $ins_data['session_id'] = $sessionid;
+        $ins_data['survey_map_id'] = $survey_map_id;
+        $ins_data['location'] = $u_location;
+        $ins_data['created'] = strtotime('now');
+        $ins_data['changed'] = 0;
 
-        $query = "INSERT into respondants(resp_id, partner_id, session_id, survey_id, adid, outcome, location) values (:id, :partner_id, :sessionid, :survey_id, 'Partner',  'DROPOFF', :u_location); ";
-        $conn->prepare($query)->execute($ins_data);
-
-        $query = "select survey_link from survey where survey_id = ? ";
+        $query = "INSERT INTO respondents_term(id, resp_id, partner_id, session_id, survey_id, adid, outcome, location, created, changed) 
+        VALUES (NULL, :resp_id, :partner_id, :session_id, :survey_map_id, 'Partner',  'DROPOFF', :location, :created, :changed) ";
         $result = $conn->prepare($query);
-        $result->execute(array($survey_id));
-        $result1 = $result->fetchAll(PDO::FETCH_ASSOC);
+        $result->execute($ins_data);
 
-        $survey_link = $result1[0]['survey_link'];
+        $survey_link = getSurveyUrl($conn, $survey_map_id);
     } catch (PDOException $e) {
       echo 'Error: ' . $e->getMessage() . '<br />';
     }
 
-    $conn = null;
     $survey_link = str_replace("xxxx", $id, $survey_link);
     Redirect($survey_link, false);
 }
+
+
+function getSurveyUrl($conn, $survey_map_id) {
+  $query = "SELECT sl.field_survey_link_value AS survey_link  
+                  FROM node__field_survey_link AS sl
+                  JOIN node__field_survey_name as sn ON sl.entity_id = sn.field_survey_name_target_id
+                  WHERE sn.entity_id = ? ";
+  $result = $conn->prepare($query);
+  $result->execute([$survey_map_id]);
+  $result1 = $result->fetchAll(PDO::FETCH_ASSOC);
+  $survey_link = $result1[0]['survey_link'];
+  return $survey_link;
+}
+
+function getRedurectUrl($conn, $survey_map_id) {
+  $query = "SELECT qf.field_quota_full_redirect_value AS qf_link  
+                  FROM node__field_quota_full_redirect AS qf
+                  WHERE qf.entity_id = ? ";
+  $result = $conn->prepare($query);
+  $result->execute([$survey_map_id]);
+  $result1 = $result->fetchAll(PDO::FETCH_ASSOC);
+  $survey_link = $result1[0]['qf_link'];
+  return $survey_link;
+}
+
 ?>
--- web/surveys/srv_end.php
+++ web/surveys/srv_end.php
@@ -1,21 +1,21 @@
 <?php
-session_start();
 require_once 'connection.php';
 
+session_start();
 $arr['id'] = (string) filter_input(INPUT_GET, 'id');
 $arr['status'] = (string) filter_input(INPUT_GET, 'status');
 
-function Redirect($url, $permanent = false) {
-    header('Location: ' . $url, true, $permanent ? 301 : 302);
-    exit();
+function Redirect ($url, $permanent = false) {
+  header('Location: ' . $url, true, $permanent ? 301 : 302);
+  exit();
 }
 
 if ($arr['id'] == "xxxx" || !$arr['id']) {
-    echo "error";
-    exit;
+  echo "error";
+  exit;
 }
 
-$query = "select partner_id, survey_id, adid from respondants where resp_id= ? ";
+$query = "select partner_id, survey_id, adid from respondents_term where resp_id= ? ";
 $result = $conn->prepare($query);
 $result->execute(array($arr['id']));
 $result1 = $result->fetchAll(PDO::FETCH_ASSOC);
@@ -24,104 +24,142 @@ $arr['survey_id'] = $result1[0]['survey_id'];
 $arr['partner_id'] = $result1[0]['partner_id'];
 $arr['adid'] = $result1[0]['adid'];
 
+/* Validate required survey for a partner completed - Start here */
+$query = "SELECT com.field_completes_value AS cv, sr.field_total_survey_required_value AS rs  
+          FROM node__field_total_survey_required AS sr
+          JOIN node__field_completes AS com ON sr.entity_id = com.entity_id
+          WHERE sr.entity_id = ? ";
+$result = $conn->prepare($query);
+$result->execute([$arr['survey_id']]);
+$result1 = $result->fetchAll(PDO::FETCH_ASSOC);
+
+if ($result1[0]['cv'] > $result1[0]['rs']) {
+  $survey_link = getRedurectUrl($conn, $arr['survey_id']);
+  // $survey_link = str_replace("xxxx", $id, $survey_link);
+  Redirect($survey_link, false);
+  die;  
+}
+/* Validate required survey for a partner completed - End here */
+
 switch ($arr['status']) {
-    case "comp":
-        $query = "UPDATE survey JOIN respondants
-                SET completes = CASE 
-                	WHEN respondants.changed IS NULL THEN completes + 1 
-                  else 
-                    completes
-                END,
-                  is_live = CASE
-                      WHEN completes >= current_target THEN 0
-                	ELSE 
-                      is_live
-                END
-                
-                WHERE survey.survey_id = :survey_id AND respondants.resp_id = :id ;";
-                        $query .= "UPDATE respondants
-                SET outcome= CASE
-                   WHEN changed IS NULL THEN 'Complete'
-                   ELSE 
-                     outcome
-                END
-                WHERE resp_id = :id ;";
-        break;
-    case "term":
-        $query = "UPDATE survey JOIN respondants
-            set terminates = case 
-            	WHEN respondants.changed IS NULL THEN terminates + 1 
-            	else terminates
-            END,
-            is_live = CASE
-            WHEN completes >= current_target THEN 0
-            	ELSE is_live
-            END
-            WHERE survey.survey_id = :survey_id AND respondants.resp_id = :id ;";
-                    $query .= "UPDATE respondants
-            SET outcome= CASE
-               WHEN changed IS NULL THEN 'Terminate'
-               ELSE outcome
-            END
-            WHERE resp_id = :id ;";
-                    break;
-                case "quot":
-                    $query = "UPDATE survey JOIN respondants
-            set quota = case 
-            	WHEN respondants.changed IS NULL THEN quota + 1 
-            	else quota
-            END,
-            is_live = CASE
-            WHEN completes >= current_target THEN 0
-            	ELSE is_live
-            END
-            WHERE survey.survey_id = :survey_id AND respondants.resp_id = :id ;";
-                    $query .= "UPDATE respondants
-            SET outcome= CASE
-               WHEN changed IS NULL THEN 'Quota-Full'
-               ELSE outcome
-            END
-            WHERE resp_id = :id ;";
-                    break;
-                case "test":
-                    $query .= "UPDATE respondants
-            SET outcome= CASE
-               WHEN changed IS NULL THEN 'Test'
-               ELSE outcome
-            END
-            WHERE resp_id = :id ;";
+  case "comp":
+    $query = "UPDATE respondents_term AS res 
+      JOIN node__field_completes AS com ON res.survey_id = com.entity_id
+      SET com.field_completes_value = CASE 
+        WHEN res.changed IS NULL THEN com.field_completes_value + 1 
+        ELSE 
+          com.field_completes_value
+        END
+    WHERE com.entity_id = ? AND res.resp_id = ? ;";
+
+    $query .= "UPDATE respondents_term AS res
+      SET res.changed = ?
+      SET res.outcome = CASE
+        WHEN res.changed IS NULL THEN 'Complete'
+        ELSE 
+          res.outcome
+        END
+      WHERE res.resp_id = ? ;";
+  break;
+
+  case "term":
+    $query = "UPDATE respondents_term AS res      
+    JOIN node__field_terminates AS ter ON res.survey_id = ter.entity_id
+      set ter.field_terminates_value = case 
+        WHEN res.changed IS NULL THEN ter.field_terminates_value + 1 
+          else ter.field_terminates_value
+        END
+      WHERE ter.entity_id = ? AND res.resp_id = ? ;";
+
+    $query .= "UPDATE respondents_term AS res
+      SET res.outcome= CASE 
+        WHEN res.changed IS NULL THEN 'Terminate'
+        ELSE 
+          res.outcome
+        END
+      WHERE res.resp_id = ? ;";
+  break;
+
+  case "quot":
+    $query = "UPDATE respondents_term AS res
+      JOIN node__field_quota AS quo ON res.survey_id = quo.entity_id
+      SET quo.field_quota_value = CASE 
+        WHEN res.changed IS NULL THEN quo.field_quota_value + 1 
+        ELSE 
+          quo.field_quota_value
+        END
+      WHERE quo.entity_id = ? AND res.resp_id = ? ;";
+
+    $query .= "UPDATE respondents_term AS res
+      SET res.outcome = CASE
+        WHEN res.changed IS NULL THEN 'Quota-Full'
+        ELSE 
+          res.outcome
+        END
+      WHERE res.resp_id = :id ;";
+  break;
+
+  case "test":
+    $query .= "UPDATE respondents_term as res
+      SET res.outcome = CASE
+      WHEN res.changed IS NULL THEN 'Test'
+      ELSE 
+        res.outcome
+      END
+      WHERE res.resp_id = :id ;";
 }
 
-$result2 = $conn->prepare($query);
-$result2->bindParam(':survey_id', $arr['survey_id'], PDO::PARAM_INT);
-$result2->bindValue(':id', $arr['id'], PDO::PARAM_STR);
-$result2->execute();
-// print_r($result2->debugDumpParams());
+try {
+$result = $conn->prepare($query);
+$result->execute([$arr['survey_id'], $arr['id'], $arr['id']]);
+// print_r($result->debugDumpParams());
 
-if ($result2) {
+  if ($result) {
     echo "thank you for your time";
     if ($arr['adid'] == 'Partner') {
-    switch ($arr['status']) {
-        case 'comp' : $redirect = 'comp_redirect';
-            break;
-        case 'term' : $redirect = 'term_redirect';
-            break;
-        case 'quot' : $redirect = 'quot_redirect';
-    }
-    
-    $query = 'SELECT '. $redirect .' FROM partner WHERE survey_id = ? ';
-    $result = $conn->prepare($query);
-    $result->execute(array($arr['survey_id']));
-    $row = $result->fetchAll(PDO::FETCH_ASSOC);
-    $redirect = $row[0]['comp_redirect'];
+      switch ($arr['status']) {
+        case 'comp' :
+          $table = 'node__field_completes_redirect'; 
+          $redirect = 'field_completes_redirect_value';
+          break;
+        case 'term' : 
+          $table = 'node__field_terminates_redirect';
+          $redirect = 'field_terminates_redirect_value';
+          break;
+        case 'quot' : 
+          $table = 'node__field_quota_full_redirect';
+          $redirect = 'field_quota_full_redirect_value';
+      }
 
-    $redirect = str_replace("xxxx", $arr['partner_id'], $redirect);
-    Redirect($redirect, false);
+      $query = 'SELECT '. $redirect .' FROM '. $table .' WHERE entity_id = ? ';
+      $result = $conn->prepare($query);
+      $result->execute([$arr['survey_id']]);
+      $row = $result->fetchAll(PDO::FETCH_ASSOC);
+      $redirect = $row[0][$redirect];
+
+      $redirect = str_replace("xxxx", $arr['partner_id'], $redirect);
+      Redirect($redirect, false);
+    }
+  }
+
+} catch (PDOException $e) {
+  echo 'Error: ' . $e->getMessage() . '<br />';
+}
+
+
+function getRedurectUrl($conn, $survey_map_id) {
+  $query = "SELECT qf.field_quota_full_redirect_value AS qf_link  
+                  FROM node__field_quota_full_redirect AS qf
+                  WHERE qf.entity_id = ? ";
+  $result = $conn->prepare($query);
+  $result->execute([$survey_map_id]);
+  $result1 = $result->fetchAll(PDO::FETCH_ASSOC);
+  $survey_link = $result1[0]['qf_link'];
+  return $survey_link;
 }
 
 session_destroy();
 ?>
-
 
 <html>
     <head>
@@ -132,11 +170,9 @@ session_destroy();
     </head>
     <body class="homepage is-preload">
         <div id="page-wrapper">
-
             <!-- Header -->
             <section id="header">
                 <div class="container">
-
                     <!-- Logo -->
                     <h1 id="logo"><a href="/">Thank You </a></h1>
                     <header><h2>Your responses have been submitted and are being checked.</h2></header>
