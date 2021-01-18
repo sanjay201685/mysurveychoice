112c2abd545d6e239017dda885361b8c9fbb68d4
Sanjay <sky.creatrix@gmail.com>
2021-01-18 22:34:49 +0530

    Added settings.php file configuration

--- web/surveys/connection.php
+++ web/surveys/connection.php
@@ -1,10 +1,12 @@
 <?php
 
+require_once '../sites/default/settings.php';
+
 try {
-  $hostname = "localhost";
-  $username = "root"; 
-  $password = "root";
-  $dbname = "mysurveychoice";
+  $hostname = $databases['default']['host'];
+  $username = $databases['default']['username']; 
+  $password = $databases['default']['password'];
+  $dbname = $databases['default']['default']['database'];
 
   $conn = new PDO("mysql:host=$hostname; dbname=$dbname", $username, $password);
 } catch (PDOException $e){
